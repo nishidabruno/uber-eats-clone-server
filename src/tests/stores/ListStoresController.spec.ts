eee3ceb28dadd155e7cdd6e2135aecdc2405de15
Bruno Nishida <bruno.nishida.d.s@gmail.com>
2021-10-13 15:16:48 +0900

    fix ListStores integration test

--- src/tests/stores/ListStoresController.spec.ts
+++ src/tests/stores/ListStoresController.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { Connection } from 'typeorm';
 
 import { createTypeormConnection } from '@config/typeorm';
+import { StoresCoordinateRepository } from '@infra/database/typeorm/repositories/StoresCoodinateRepository';
 import { StoresRepository } from '@infra/database/typeorm/repositories/StoresRepository';
 import { UsersRepository } from '@infra/database/typeorm/repositories/UsersRepository';
 import { app } from '@infra/http/app';
@@ -10,6 +11,7 @@ let connection: Connection;
 
 let usersRepository: UsersRepository;
 let storesRepository: StoresRepository;
+let storesCoordinatesRepository: StoresCoordinateRepository;
 
 describe('ListStoresController', () => {
   beforeAll(async () => {
@@ -19,6 +21,7 @@ describe('ListStoresController', () => {
     usersRepository = new UsersRepository();
     storesRepository = new StoresRepository();
     usersRepository = new UsersRepository();
+    storesCoordinatesRepository = new StoresCoordinateRepository();
   });
 
   afterAll(async () => {
@@ -31,17 +34,18 @@ describe('ListStoresController', () => {
       full_name: 'Valid name',
       email: 'valid@email.com',
       password: 'valid-password',
-      avatar: 'http://valid-imgurl.com',
     });
 
     const user = await usersRepository.findByEmail('valid@email.com');
 
+    const storeCoordinates = await storesCoordinatesRepository.create({
+      longitude: 123,
+      latitude: 456,
+    });
+
     await storesRepository.create({
       address: 'Valid address',
-      location: {
-        type: 'Point',
-        coordinates: [41.40338, 2.17403],
-      },
+      coordinates_id: storeCoordinates.id,
       delivery_fee: 0,
       delivery_time: 90,
       categories: [],
@@ -49,13 +53,13 @@ describe('ListStoresController', () => {
       opening_time_weekend: '8-17',
       opening_time_workweek: '8-18',
       user_id: user!.id,
+      image: 'validImage',
     });
 
-    const response = await request(app).get('/stores');
+    const response = await request(app).get('/stores/');
 
     expect(response.statusCode).toEqual(200);
     expect(response.body).toHaveLength(1);
-    expect(response.body[0].location.coordinates).toEqual([41.40338, 2.17403]);
     expect(response.body[0]).toHaveProperty('categories');
     expect(response.body[0]).toHaveProperty('products');
   });
