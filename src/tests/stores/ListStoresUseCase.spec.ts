68c1738b8ae673b48850f28d2882f312fbbf0f02
Bruno Nishida <bruno.nishida.d.s@gmail.com>
2021-10-13 18:44:02 +0900

    fix ListStoresUseCase unit test

--- src/tests/stores/ListStoresUseCase.spec.ts
+++ src/tests/stores/ListStoresUseCase.spec.ts
@@ -13,10 +13,7 @@ describe('ListStoresUseCase', () => {
   it('should be able to list all stores', async () => {
     await storesRepositoryInMemory.create({
       address: 'Valid address',
-      location: {
-        type: 'Point',
-        coordinates: [123, 456],
-      },
+      coordinates_id: '123',
       delivery_fee: 0,
       delivery_time: 90,
       categories: [],
@@ -24,14 +21,12 @@ describe('ListStoresUseCase', () => {
       opening_time_weekend: '8-17',
       opening_time_workweek: '8-18',
       user_id: '123',
+      image: 'validImage',
     });
 
     await storesRepositoryInMemory.create({
       address: 'Valid address',
-      location: {
-        type: 'Point',
-        coordinates: [123, 456],
-      },
+      coordinates_id: '123',
       delivery_fee: 0,
       delivery_time: 90,
       categories: [],
@@ -39,6 +34,7 @@ describe('ListStoresUseCase', () => {
       opening_time_weekend: '8-17',
       opening_time_workweek: '8-18',
       user_id: '123',
+      image: 'validImage',
     });
 
     const stores = await listStoresUseCase.execute();
