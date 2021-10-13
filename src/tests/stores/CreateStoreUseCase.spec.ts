c8dd80c13d00b3de4e1d798931b30a89c8dd5d2a
Bruno Nishida <bruno.nishida.d.s@gmail.com>
2021-10-13 18:40:22 +0900

    fix CreateStoreUseCase unit test

--- src/tests/stores/CreateStoreUseCase.spec.ts
+++ src/tests/stores/CreateStoreUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { AppError } from '@errors/AppError';
+import { StorageProviderInMemory } from '@providers/in-memory/StorageProviderInMemory';
 import { CategoriesRepositoryInMemory } from '@repositories/in-memory/CategoriesRepositoryInMemory';
+import { StoresCoordinatesRepositoryInMemory } from '@repositories/in-memory/StoresCoordinatesRepositoryInMemory';
 import { StoresRepositoryInMemory } from '@repositories/in-memory/StoresRepositoryInMemory';
 import { UsersRepositoryInMemory } from '@repositories/in-memory/UsersRepositoryInMemory';
 import { CreateStoreUseCase } from '@useCases/stores/CreateStoreUseCase';
@@ -8,16 +10,23 @@ let storesRepositoryInMemory: StoresRepositoryInMemory;
 let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
 let createStoreUseCase: CreateStoreUseCase;
+let storageProvider: StorageProviderInMemory;
+let storesCoordinatesRepository: StoresCoordinatesRepositoryInMemory;
 
 describe('CreateStoreUseCase', () => {
   beforeEach(() => {
     storesRepositoryInMemory = new StoresRepositoryInMemory();
     categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
     usersRepositoryInMemory = new UsersRepositoryInMemory();
+    storageProvider = new StorageProviderInMemory();
+    storesCoordinatesRepository = new StoresCoordinatesRepositoryInMemory();
+
     createStoreUseCase = new CreateStoreUseCase(
       storesRepositoryInMemory,
       usersRepositoryInMemory,
-      categoriesRepositoryInMemory
+      categoriesRepositoryInMemory,
+      storageProvider,
+      storesCoordinatesRepository
     );
   });
 
@@ -26,8 +35,11 @@ describe('CreateStoreUseCase', () => {
       full_name: 'Valid name',
       email: 'validemail@email.com',
       password: 'valid-password',
-      avatar: 'http://valid-imgurl.com',
     });
+    const spyOnCoordinatesRepository = jest.spyOn(
+      storesCoordinatesRepository,
+      'create'
+    );
 
     const category = await categoriesRepositoryInMemory.create({
       image: 'Valid image',
@@ -38,12 +50,14 @@ describe('CreateStoreUseCase', () => {
       'validemail@email.com'
     );
 
+    const coordinates = {
+      longitude: 35.6847875,
+      latitude: 139.710875,
+    };
+
     const store = await createStoreUseCase.execute({
       address: 'Valid address',
-      location: {
-        long: 35.6847875,
-        lat: 139.710875,
-      },
+      coordinates,
       delivery_fee: 0,
       delivery_time: 90,
       categories_id: [category.id],
@@ -51,22 +65,25 @@ describe('CreateStoreUseCase', () => {
       opening_time_weekend: '8-17',
       opening_time_workweek: '8-18',
       user_id: user!.id,
+      image: 'validImage',
     });
 
     expect(store).toHaveProperty('id');
     expect(store).toHaveProperty('categories');
-    expect(store.location.coordinates).toHaveLength(2);
+    expect(store).toHaveProperty('coordinates_id');
+    expect(spyOnCoordinatesRepository).toHaveBeenCalledTimes(1);
     expect(store.categories[0]).toHaveProperty('id');
   });
 
   it('should not be able to create a store if the provided user id does not exist', async () => {
+    const coordinates = {
+      longitude: 35.6847875,
+      latitude: 139.710875,
+    };
     await expect(
       createStoreUseCase.execute({
         address: 'Valid address',
-        location: {
-          long: 35.6847875,
-          lat: 139.710875,
-        },
+        coordinates,
         delivery_fee: 0,
         delivery_time: 90,
         categories_id: ['123'],
@@ -74,6 +91,7 @@ describe('CreateStoreUseCase', () => {
         opening_time_weekend: '8-17',
         opening_time_workweek: '8-18',
         user_id: 'Invalid user id',
+        image: 'validImage',
       })
     ).rejects.toEqual(new AppError('The provided user does not exist.', 400));
   });
