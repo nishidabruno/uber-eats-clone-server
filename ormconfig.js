ace85998dad757203d6786f50c85a2e297c1ac58
Bruno Nishida <bruno.nishida.d.s@gmail.com>
2021-10-20 17:06:02 +0900

    typeorm set ssl to true

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,9 @@ module.exports = {
     "host": process.env.DB_HOST,
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
+    "extra": {
+      ssl: true,
+    },
     "migrations": [process.env.TYPEORM_MIGRATION_DIR],
     "entities": [process.env.TYPEORM_ENTITIES],
     "cli": {
