33fd8671e4db350c3d2e82c8c11fdccf1165acbe
Bruno Nishida <bruno.nishida.d.s@gmail.com>
2021-10-20 17:13:57 +0900

    fix ssl authorization

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,19 @@
 module.exports = {
-    "type": process.env.DB_TYPE,
-    "database": process.env.DB_NAME,
-    "port": process.env.DB_PORT,
-    "host": process.env.DB_HOST,
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "extra": {
-      ssl: true,
+    type: process.env.DB_TYPE,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
     },
-    "migrations": [process.env.TYPEORM_MIGRATION_DIR],
-    "entities": [process.env.TYPEORM_ENTITIES],
-    "cli": {
-      "migrationsDir": process.env.TYPEORM_MIGRATIONS_DIR
+    migrations: [process.env.TYPEORM_MIGRATION_DIR],
+    entities: [process.env.TYPEORM_ENTITIES],
+    cli: {
+      migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
     }
 }
